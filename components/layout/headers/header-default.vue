836c266632309ab86051a164b9c0032dc24c115c
Aleix <aleix.martinez@talkualfoods.com>
2024-02-15 08:50:59 +0100

    Create component header

---
+++ components/layout/headers/header-default.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <Section :class="size">
+      <Container>
+        <slot />
+      </Container>
+    </Section>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+  size: {
+    type: String,
+    default: null
+  }
+})
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ components/layout/sections/section.vue
@@ -0,0 +1,17 @@
+<template>
+  <section
+    class="bg-cover"
+    :class="{'bg-cover py-24 px-6': isMedium}"
+  >
+    <slot />
+  </section>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  isMedium: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
