d2b3a202eb4164e3bdb9f13031afade535c835d3
luis-ramirez-talkualfoods <luis.ramirez@talkualfoods.com>
2024-02-15 17:03:00 +0000

    Rename component Field to TKField

---
+++ components/talkual-ui/TKField.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+    errors?: object[];
+}>();
+</script>
+<template>
+    <div class="flex flex-col flex-wrap">
+        <slot/>
+        <small
+            v-for="error in errors"
+            :key="error"
+            class="text-red-primary text-xs"
+        >
+            {{ error }}
+        </small>
+    </div>
+</template>
