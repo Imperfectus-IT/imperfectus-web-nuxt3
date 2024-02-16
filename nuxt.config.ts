744e8c6ebf435ad079de53680d58afe63fda48e3
luis-ramirez-talkualfoods <luis.ramirez@talkualfoods.com>
2024-02-16 09:20:48 +0000

    Add rating component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,112 +2,113 @@
 import * as path from "path";
 
 export default defineNuxtConfig({
-  app: {
-    head: {
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css?display=swap",
+    app: {
+        head: {
+            link: [
+                {
+                    rel: "stylesheet",
+                    href: "https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css?display=swap",
+                },
+            ],
         },
-      ],
     },
-  },
-  runtimeConfig: {
-    baseUrl: process.env.BASE_URL,
-    public: {
-      baseUrl: process.env.BASE_URL,
+    runtimeConfig: {
+        baseUrl: process.env.BASE_URL,
+        public: {
+            baseUrl: process.env.BASE_URL,
+        },
     },
-  },
-  routeRules: {
-    "/api/": { proxy: process.env.STRAPI_URL, pathRewrite: { "^/api/": "" } },
-    "/uploads/": { proxy: process.env.STRAPI_URL },
-  },
-  components: [
-    {
-      path: "~/components",
-      pathPrefix: false,
+    routeRules: {
+        "/api/": {proxy: process.env.STRAPI_URL, pathRewrite: {"^/api/": ""}},
+        "/uploads/": {proxy: process.env.STRAPI_URL},
     },
-  ],
-  imports: {
-    dirs: ["composables/**"],
-  },
-  typescript: {
-    typeCheck: false,
-  },
-  devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "nuxt-zod-i18n",
-    "@nuxtjs/i18n",
-    "nuxt-primevue",
-    "@nuxt/image",
-    "nuxt-svgo",
-    "@nuxtjs/eslint-module",
-    "@nuxtjs/strapi",
-  ],
-  primevue: {
-    options: { unstyled: true },
-    importPT: {
-      as: "TalkualUI",
-      from: path.resolve(__dirname, "./presets/talkual-ui/"),
-    },
-    components: {
-      include: [
-        "Button",
-        "Card",
-        "Carousel",
-        "Checkbox",
-        "Dialog",
-        "Divider",
-        "Dropdown",
-        "Image",
-        "InputText",
-        "Multiselect",
-        "Password",
-        "ProgressBar",
-        "Sidebar",
-        "Toolbar",
-      ],
-    },
-  },
-  tailwindcss: {
-    cssPath: "~/assets/scss/tailwind.scss",
-  },
-  i18n: {
-    baseUrl: process.env.BASE_URL,
-    locales: [
-      {
-        name: "Català",
-        code: "ca",
-        file: "ca-ES.ts",
-        iso: "ca-ES",
-      },
-      {
-        name: "Español",
-        code: "es",
-        file: "es-ES.ts",
-        iso: "es-ES",
-      },
+    components: [
+        {
+            path: "~/components",
+            pathPrefix: false,
+        },
     ],
-    lazy: true,
-    langDir: "lang/",
-    strategy: "prefix_except_default",
-    defaultLocale: "es",
-  },
-  zodI18n: {
-    localeCodesMapping: {
-      "es-ES": "es",
-      "ca-ES": "ca",
+    imports: {
+        dirs: ["composables/**"],
     },
-  },
-  strapi: {
-    prefix: "/api/",
-    cookie: {
-      path: "/",
-      maxAge: 14 * 24 * 60 * 60,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: true,
+    typescript: {
+        typeCheck: false,
     },
-    version: "v3",
-  },
+    devtools: {enabled: true},
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "nuxt-zod-i18n",
+        "@nuxtjs/i18n",
+        "nuxt-primevue",
+        "@nuxt/image",
+        "nuxt-svgo",
+        "@nuxtjs/eslint-module",
+        "@nuxtjs/strapi",
+    ],
+    primevue: {
+        options: {unstyled: true},
+        importPT: {
+            as: "TalkualUI",
+            from: path.resolve(__dirname, "./presets/talkual-ui/"),
+        },
+        components: {
+            include: [
+                "Button",
+                "Card",
+                "Carousel",
+                "Checkbox",
+                "Dialog",
+                "Divider",
+                "Dropdown",
+                "Image",
+                "InputText",
+                "Multiselect",
+                "Password",
+                "ProgressBar",
+                "Rating",
+                "Sidebar",
+                "Toolbar",
+            ],
+        },
+    },
+    tailwindcss: {
+        cssPath: "~/assets/scss/tailwind.scss",
+    },
+    i18n: {
+        baseUrl: process.env.BASE_URL,
+        locales: [
+            {
+                name: "Català",
+                code: "ca",
+                file: "ca-ES.ts",
+                iso: "ca-ES",
+            },
+            {
+                name: "Español",
+                code: "es",
+                file: "es-ES.ts",
+                iso: "es-ES",
+            },
+        ],
+        lazy: true,
+        langDir: "lang/",
+        strategy: "prefix_except_default",
+        defaultLocale: "es",
+    },
+    zodI18n: {
+        localeCodesMapping: {
+            "es-ES": "es",
+            "ca-ES": "ca",
+        },
+    },
+    strapi: {
+        prefix: "/api/",
+        cookie: {
+            path: "/",
+            maxAge: 14 * 24 * 60 * 60,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: true,
+        },
+        version: "v3",
+    },
 });
---
+++ presets/talkual-ui/rating/index.ts
@@ -0,0 +1,92 @@
+export default {
+    root: ({props}) => ({
+        class: [
+            'relative',
+
+            // Flex & Alignment
+            'flex items-center',
+            'gap-2',
+
+            // Misc
+            {
+                'opacity-60 select-none pointer-events-none cursor-default': props.disabled
+            }
+        ]
+    }),
+    cancelitem: ({context}) => ({
+        class: [
+            // Flex & Alignment
+            'inline-flex items-center',
+
+            //State
+            {
+                'outline-none ring ring-green-tertiary': context.focused
+            },
+
+            // Misc
+            'cursor-pointer'
+        ]
+    }),
+    cancelicon: {
+        class: [
+            // Size
+            'w-5 h-5',
+
+            // Color
+            'text-red-primary',
+
+            // State
+            'hover:text-red-primary',
+
+            // Transition
+            'transition duration-200 ease-in'
+        ]
+    },
+    item: ({props, context}) => ({
+        class: [
+            // Flex & Alignment
+            'inline-flex items-center',
+
+            // State
+            {
+                'outline-none ring-none': context.focused
+            },
+
+            // Misc
+            {
+                'cursor-pointer': !props.readonly,
+                'cursor-default': props.readonly
+            }
+        ]
+    }),
+    officon: ({props}) => ({
+        class: [
+            // Size
+            'w-5 h-5',
+
+            // Color
+            'text-green-tertiary',
+
+            // State
+            {'hover:text-green-tertiary': !props.readonly},
+
+            // Transition
+            'transition duration-200 ease-in'
+        ]
+    }),
+    onicon: ({props}) => ({
+        class: [
+            // Size
+            'w-5 h-5',
+
+            // Color
+            'text-yellow-primary',
+
+            // State
+            {'hover:text-yellow-primary': !props.readonly},
+
+            // Transition
+            'transition duration-200 ease-in'
+        ]
+    })
+};
