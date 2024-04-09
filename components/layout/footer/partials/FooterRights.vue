d286065bac691f18954d7a7ddedceea20164b22e
luis-ramirez-talkualfoods <luis.ramirez@talkualfoods.com>
2024-04-09 17:41:31 +0100

    Fix order for different size to elements in footer

--- components/layout/footer/partials/FooterRights.vue
+++ components/layout/footer/partials/FooterRights.vue
@@ -18,15 +18,15 @@ const logoItems = [
 ]
 </script>
 <template>
-  <div class="bg-green-primary text-green-tertiary pt-2 lg:flex lg:justify-around lg:items-center lg:h-24">
-    <h4 class="text-[30px] font-recoleta-regular text-center lg:text-2xl">
-      {{ $t("footerLegal.claim") }}
-    </h4>
-    <div class="text-center mt-2">
+  <div class="grid grid-cols-1 lg:grid-cols-3 bg-green-primary text-green-tertiary pt-2 lg:justify-around lg:items-center lg:h-24">
+    <div class="order-2 lg:order-1 text-center mt-2">
       <span class="mr-2">{{ new Date().getFullYear() }} TALKUAL. </span>
       <span>All rights reserved.</span>
     </div>
-    <div class="bg-green-primary flex items-center gap-5 p-4 lg:justify-center">
+    <h4 class="order-1 lg:order-2 text-[30px] font-recoleta-regular text-center lg:text-2xl">
+      {{ $t("footerLegal.claim") }}
+    </h4>
+    <div class="order-3 bg-green-primary flex items-center gap-5 p-4 lg:justify-center">
       <NuxtImg v-for ="logo in logoItems" :key="logo.id" :src="logo.src" :alt="logo.alt" width="120" loading="lazy" format="webp" />
     </div>
   </div>
