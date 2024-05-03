3cc939473667630465131fe994c7b674ca6b9690
luis-ramirez-talkualfoods <luis.ramirez@talkualfoods.com>
2024-05-03 16:48:00 +0100

    Move rules to prepend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,14 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-    {
-        files: ['*.vue'],
-        rules: {
-            "vue/multi-word-component-names": "off",
-            "vue/no-multiple-template-root": "off",
-        }
-    },
-)
+export default withNuxt()
     .prepend(
-
+        {
+            files: ['*.vue'],
+            rules: {
+                "vue/multi-word-component-names": "off",
+                "vue/no-multiple-template-root": "off",
+            }
+        },
     )
     // Override some rules in a specific config, based on their name
     .override('nuxt/typescript', {
