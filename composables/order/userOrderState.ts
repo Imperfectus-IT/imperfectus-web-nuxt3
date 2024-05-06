db5aa86ae4e876df772ecb2ee5b671aad72db572
Roger <roger.gutierrez@talkualfoods.com>
2024-05-06 17:20:56 +0200

    Fix padding and margin top on landing page

--- composables/order/userOrderState.ts
+++
@@ -1,9 +0,0 @@
-import type { Order } from '~/composables/admin/orders/types/OrderType.ts'
-
-export const userOrderState = () => {
-  const order: Ref<Order | null> = useState('orders', () => null)
-
-  return {
-    order,
-  }
-}
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
       <MobileNavbarHeader class="lg:hidden bg-beige-primary" />
       <DesktopNavbarHeader class="hidden lg:block bg-beige-primary mb-0" />
     </section>
-    <div class="pt-10 lg:pt-24 text-green-tertiary">
+    <div class="text-green-tertiary">
       <slot />
     </div>
     <LazyFooterDefault />
