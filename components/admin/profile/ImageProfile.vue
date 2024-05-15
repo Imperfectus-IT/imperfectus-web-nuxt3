2f70a7ac3a8cba35c348d1c76ec76f47b9c62fd3
Roger <roger.gutierrez@talkualfoods.com>
2024-05-15 12:12:05 +0200

    remove image props

--- components/admin/profile/ImageProfile.vue
+++ components/admin/profile/ImageProfile.vue
@@ -4,8 +4,6 @@
       class="mt-3 lg:w-[330px] lg:h-[335px]"
       src="/images/admin/my-account/mi-cuenta-2.webp"
       alt="Mi cuenta 2"
-      width="330"
-      height="335"
       loading="lazy"
       fit="cover"
       format="webp"
