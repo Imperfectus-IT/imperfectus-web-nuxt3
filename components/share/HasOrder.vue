c482761ca1ab87e10103f1b10175db7b07d9ff79
Aleix <aleix.martinez@talkualfoods.com>
2024-11-21 09:00:51 +0100

    Fix colors and font size

--- components/share/HasOrder.vue
+++ components/share/HasOrder.vue
@@ -21,30 +21,42 @@ const discardOrder = () => {
 <template>
   <div
     v-if="isHasOrderVisible"
-    class="flex items-center justify-center  bg-green-tertiary text-white"
+    class="bg-green-tertiary"
   >
-    <span class="text-white-primary">👋 {{ t('hasOrder.message') }}</span>
+    <div class="flex items-center justify-center py-2">
+      <span class="text-beige-primary">👋 {{ t('hasOrder.message') }}</span>
 
-    <NuxtLinkLocale
-      to="order"
-    >
+      <NuxtLinkLocale
+        to="order"
+      >
+        <Button
+          class="ml-5 bg-transparent text-green-primary underline"
+          size="small"
+          variant="text"
+          :pt="{
+            root: {
+              class: 'text-[16px] font-bold',
+            },
+          }"
+        >
+          {{ t('hasOrder.next') }}
+        </Button>
+      </NuxtLinkLocale>
+
       <Button
-        class="ml-5 bg-transparent text-green-primary underline"
+        class="ml-4 bg-transparent text-beige-primary underline"
         size="small"
         variant="text"
+        :pt="{
+          root: {
+            class: 'text-[16px]',
+          },
+        }"
+        @click="discardOrder"
       >
-        {{ t('hasOrder.next') }}
+        {{ t('hasOrder.discard') }}
       </Button>
-    </NuxtLinkLocale>
-
-    <Button
-      class="ml-8 bg-transparent text-white-primary underline"
-      size="small"
-      variant="text"
-      @click="discardOrder"
-    >
-      {{ t('hasOrder.discard') }}
-    </Button>
+    </div>
   </div>
 </template>
 
