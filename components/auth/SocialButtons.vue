b7017b508ce25378b316ba8317d58f19529285fe
Roger <roger.gutierrez@talkualfoods.com>
2024-11-29 00:59:00 +0100

    Change googleurl access

--- components/auth/SocialButtons.vue
+++ components/auth/SocialButtons.vue
@@ -2,15 +2,22 @@
 defineProps<{
   googleLabel: string
 }>()
-const googleConnectUrl = `${useRuntimeConfig().public.baseUrl}/api/connect/google`
+const googleConnectUrl = `${useRuntimeConfig().public.STRAPI_URL}/api/connect/google`
+
+const handleClick = () => {
+  window.location.href = googleConnectUrl
+}
 
 console.log(useRuntimeConfig().public.baseUrl)
 </script>
 
 <template>
   <div class="flex justify-center">
-    <NuxtLink :to="googleConnectUrl">
-      <GoogleLoginButton :label="googleLabel" />
-    </NuxtLink>
+    <!--    <NuxtLink :to="googleConnectUrl"> -->
+    <GoogleLoginButton
+      :label="googleLabel"
+      @click="handleClick"
+    />
+    <!--    </NuxtLink> -->
   </div>
 </template>
