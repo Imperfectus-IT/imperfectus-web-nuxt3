0e6f5656407c3b9b68972e496da8333fa6263eaf
Roger <roger.gutierrez@talkualfoods.com>
2024-11-28 08:35:01 +0100

    Test another proxy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,11 +40,27 @@ export default defineNuxtConfig({
       STRAPI_URL: process.env.NUXT_PUBLIC_STRAPI_URL,
       NODE_ENV: process.env.NUXT_PUBLIC_NODE_ENV,
     },
+    proxy: {
+      options: [
+        {
+          target: process.env.STRAPI_URL,
+          pathFilter: ['/api/**'],
+          pathRewrite: {
+            '^/api': '',
+          },
+        },
+        {
+          target: process.env.STRAPI_URL,
+          pathFilter: ['/uploads/**'],
+          pathRewrite: process.env.STRAPI_URL,
+        },
+      ],
+    },
   },
-  routeRules: {
-    '/api/**': { proxy: process.env.NUXT_PUBLIC_STRAPI_URL, pathRewrite: { '^/api/': '' } },
-    '/uploads/**': { proxy: process.env.NUXT_PUBLIC_STRAPI_URL },
-  },
+  // routeRules: {
+  //   '/api/**': { proxy: process.env.NUXT_PUBLIC_STRAPI_URL, pathRewrite: { '^/api/': '/admin' } },
+  //   '/uploads/**': { proxy: process.env.NUXT_PUBLIC_STRAPI_URL },
+  // },
   hooks: {
     'pages:extend'(pages) {
       pages.push({
@@ -121,6 +137,7 @@ export default defineNuxtConfig({
     'vue3-carousel-nuxt',
     '@nuxtjs/mdc',
     '@nuxtjs/device',
+    'nuxt-proxy-request',
   ],
   primevue: {
     options: {
