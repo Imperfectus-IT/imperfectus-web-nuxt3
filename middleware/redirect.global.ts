b6ce7765f44758a4fb5990e9f3591b16fa0eb7f1
Roger <roger.gutierrez@talkualfoods.com>
2024-12-18 14:42:56 +0100

    Fix backend url

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -1,10 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  // Check if the URL starts with `/api`
+  const env = useRuntimeConfig()
   if (to.path.startsWith('/api')) {
-    // Construct the new path by removing `/api`
     const newPath = to.path.replace(/^\/api/, '')
-
-    // Redirect to the backend URL
-    return navigateTo(`http://localhost:3000${newPath}`, { external: true })
+    return navigateTo(`${env.public.STRAPI_URL}${newPath}`, { external: true })
   }
 })
