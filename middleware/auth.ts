7dd76073d17aae5c91dfe1e11c411c55176b03f9
Roger <roger.gutierrez@talkualfoods.com>
2024-12-20 10:05:54 +0100

    remove fake client

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,15 +1,26 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
-  console.log('to', to)
-  console.log('from', _from)
+  console.log('AUTH!')
   const user = useStrapiUser()
-  console.log('user', user)
   const localeRoute = useLocaleRoute()
-  const route = localeRoute({ name: 'admin' })
+  const route = localeRoute({ name: 'auth-login' })
   if (!user.value && route) {
     useCookie('redirect', { path: '/' }).value = to.fullPath
-    console.log('REDIRECT TO:', route.fullPath)
-    console.log(useCookie('redirect', { path: '/' }).value)
-    user.value = { email: 'wrong'}
     return navigateTo(route.fullPath)
   }
 })
+
+// export default defineNuxtRouteMiddleware((to, _from) => {
+//   console.log('to', to)
+//   console.log('from', _from)
+//   const user = useStrapiUser()
+//   console.log('user', user)
+//   const localeRoute = useLocaleRoute()
+//   const route = localeRoute({ name: 'admin' })
+//   if (!user.value && route) {
+//     useCookie('redirect', { path: '/' }).value = to.fullPath
+//     console.log('REDIRECT TO:', route.fullPath)
+//     console.log(useCookie('redirect', { path: '/' }).value)
+//     user.value = { email: 'wrong' }
+//     return navigateTo(route.fullPath)
+//   }
+// })
